c720a70f4d8c0cb8813e2bdc9cc643327bd47c18
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-05-29 17:32:35 +0300

    add icomImageView and frame

--- tracker-app/tracker-app/CryptoTableViewCell.swift
+++ tracker-app/tracker-app/CryptoTableViewCell.swift
@@ -11,6 +11,7 @@ struct CryptoTableViewCellViewModel {
     let name:String
     let symbol:String
     let price:String
+    let iconUrl: URL?
 }
 
 class CryptoTableViewCell: UITableViewCell {
@@ -37,6 +38,13 @@ class CryptoTableViewCell: UITableViewCell {
         return label
     }()
     
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
     // Init
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -44,6 +52,7 @@ class CryptoTableViewCell: UITableViewCell {
         contentView.addSubview(nameLabel)
         contentView.addSubview(symbolLabel)
         contentView.addSubview(priceLabel)
+        contentView.addSubview(iconImageView)
     }
     
     required init?(coder: NSCoder) {
@@ -54,17 +63,25 @@ class CryptoTableViewCell: UITableViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+
+        let size: CGFloat = contentView.frame.size.height/1.1
+        iconImageView.frame = CGRect(
+            x:20,
+            y:(contentView.frame.size.height-size)/2,
+            width: size,
+            height: size)
+
         nameLabel.sizeToFit()
         priceLabel.sizeToFit()
         symbolLabel.sizeToFit()
         
         nameLabel.frame = CGRect(
-            x:25,
+            x:30+size,
             y:0,
             width: contentView.frame.size.width/2,
             height: contentView.frame.size.height/2)
         symbolLabel.frame = CGRect(
-            x:25,
+            x:30+size,
             y:contentView.frame.size.height/2,
             width: contentView.frame.size.width/2,
             height: contentView.frame.size.height/2)
--- tracker-app/tracker-app/ViewController.swift
+++ tracker-app/tracker-app/ViewController.swift
@@ -42,16 +42,24 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         APICaller.shared.getAllCryptoData { [weak self] result in
             switch result {
             case .success(let models):
-                self?.viewModels = models.compactMap({
+                self?.viewModels = models.compactMap({ model in
                     // NumberFormatter
-                    let price = $0.price_usd ?? 0
+                    let price = model.price_usd ?? 0
                     let formatter = ViewController.numberFormatter
                     let priceString = formatter.string(from: NSNumber(value: price))
                     
+                    let iconUrl = URL(
+                        string:
+                            APICaller.shared.icons.filter({ icon in
+                                icon.asset_id == model.asset_id
+                            }).first?.url ?? "")
+                    
                     return CryptoTableViewCellViewModel(
-                        name: $0.name ?? "N/A",
-                        symbol: $0.asset_id,
-                        price: priceString ?? "N/A")
+                        name: model.name ?? "N/A",
+                        symbol: model.asset_id,
+                        price: priceString ?? "N/A",
+                        iconUrl: iconUrl
+                    )
                 })
                 
                 DispatchQueue.main.async {
